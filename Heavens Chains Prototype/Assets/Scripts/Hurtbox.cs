9feb1cb02cb0a6fb1e69d4d8baefc661d8084cc4
LiuBangMoment <liuyuanqian@berkeley.edu>
2023-07-25 00:50:52 -0700

    hurtbox retooling, attackmanager also needs retooling

---
+++ Heavens Chains Prototype/Assets/Scripts/Hurtbox.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurtbox : MonoBehaviour
+{
+    [SerializeField] float damage;
+    [SerializeField] float strength;
+    public BoxCollider2D hurtbox;
+    private List<GameObject> blacklist = new List<GameObject>();
+
+    public float getDamage(){ return damage; }
+
+    public float getStrength(){ return strength; }
+
+    public void OnTriggerEnter2D(Collider2D hitHitbox)
+    {
+        GameObject hitHitboxParent;
+        bool entityInBlacklist = false;
+        foreach(GameObject entity in blacklist)
+        {
+            if(hitHitbox.gameObject.gameObject.GetComponent<CustomTag>().HasTag("Divine Weapon"))
+            {
+                if(entity == hitHitbox.gameObject.gameObject.gameObject.gameObject)
+                {
+                entityInBlacklist = true;
+                break;
+                }
+                hitHitboxParent = hitHitbox.gameObject.gameObject.gameObject.gameObject;
+            }
+            else if(hitHitbox.gameObject.gameObject.GetComponent<CustomTag>().HasTag("Entity"))
+            {
+                if(entity == hitHitbox.gameObject.gameObject.gameObject)
+                {
+                entityInBlacklist = true;
+                break;
+                }
+                hitHitboxParent = hitHitbox.gameObject.gameObject.gameObject.gameObject;
+            }
+        }
+        hitHitboxEntityTag = hitHitboxParent.GetComponent<CustomTag>();
+        hitHitboxHitboxesTag = hitHitbox.gameObject.gameObject.GetComponent<CustomTag>();
+        if(entityInBlacklist == false && !hitHitboxEntityTag.HasTag("Player"))
+        {
+            blacklist.Add(hitHitboxParent);
+            //Debug.Log("Added " + entityCollisionData.gameObject + " to blacklist");
+            //Debug.Log(">>> " + entityCollisionData.gameObject);
+            if(hitHitboxHitboxesTag.HasTag("Entity"))
+            {
+              // Debug.Log("Entity Collision" + entityCollisionData.gameObject.name);
+                hurtbox.gameObject.gameObject.gameObject.GetComponent<AttackManager>()
+                .onEntityCollide(gameObject.GetComponent<CustomTag>(), 
+                entityCollisionData.collider.gameObject, entityCollisionData.otherCollider.transform.parent.gameObject, swordDamage, swordStrength);
+            }
+            else if(hitHitboxHitboxesTag.HasTag("Weapon"))
+            {
+                //Debug.Log("Weapon Collision" + entityCollisionData.gameObject.name);
+                entityCollisionData.gameObject.GetComponent<AttackManager>()
+                .onWeaponCollide(gameObject.GetComponent<CustomTag>(), 
+                entityCollisionData.collider.gameObject, entityCollisionData.otherCollider.transform.parent.gameObject, swordDamage, (swordStrength / 2));
+            }
+            else 
+            {
+                Debug.Log("Foreign Collision");
+            }
+        }
+    }
+
+}
